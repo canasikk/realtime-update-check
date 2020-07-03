c35ad4a1f49a8c668ccd8bff6c784b074d3922dd
Can Aşık <qratipum@gmail.com>
2020-07-03 03:40:05 +0300

    Add files via upload

---
+++ canAsikk.php
@@ -0,0 +1,11 @@
+<?php
+ $latestversion = "3";
+ if(@$_GET['ca']){
+$currentversion = $_GET["ca"];
+	if ($latestversion > $currentversion) {
+		echo "GETUPDATE";
+	}
+ }
+//https://www.instagram.com/canasikk/
+            //https://github.com/canasikk
+?>
