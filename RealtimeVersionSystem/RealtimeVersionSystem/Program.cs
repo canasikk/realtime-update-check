3d251097f391b6d5befd136e433026369fbaa660
Can Aşık <qratipum@gmail.com>
2020-07-03 03:36:42 +0300

    Add files via upload

---
+++ RealtimeVersionSystem/RealtimeVersionSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealtimeVersionSystem
+{
+    static class Program
+    {
+        public static string systemVersion = "2";
+        //https://www.instagram.com/canasikk/
+        //https://github.com/canasikk
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
